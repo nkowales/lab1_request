311b0a6cd9f23455c71f6dbd84a04078c5378df2
nkowales <nkowales@nd.edu>
2015-04-29 13:47:16 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//Nathan Kowaleski
+//Lab 1 part 2
+//connect four
+//driver file
+//main.cpp
+// 4/22/15
+
+#include<iostream>
+#include"C4Col.h"
+#include"C4Board.h"
+using namespace std;
+
+
+int main()
+{
+
+C4Board c4;
+c4.play();
+
+}//end main
